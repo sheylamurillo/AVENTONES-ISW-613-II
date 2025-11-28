37f1fc4b1ee1fee871a919c709676b471faac5c2
JustinSr28 <sanchoruizjustin@gmail.com>
2025-11-28 01:04:25 -0600

    configuration default

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'AuthController::login');
+
+$routes->post('passenger/store', 'Users::storePassenger');
+$routes->post('driver/store', 'Users::storeDriver');
+
+$routes->post('auth/store', 'AuthController::authenticate');
+$routes->get('auth/registerPassenger', 'AuthController::registerPassenger');
+$routes->get('auth/registerDriver', 'AuthController::registerDriver');
+
+$routes->get('/driver/bookings', 'Driver::bookings');
+$routes->get('/passenger/searchRides', 'Passenger::searchRides');
+$routes->get('/allUsers', 'Admin::loadAllUsers');
+
+
