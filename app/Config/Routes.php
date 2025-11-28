6082e3cbceadfe97c00a7d07fcfa74e905881619
Sheyla Murillo <murillocruzsheyla36@gmail.com>
2025-11-28 12:07:07 -0600

    Feat: Added the Bookings module with routes, controller, model, and view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+//$routes->get('/', 'Home::index');
+
+
+//BOOKINGS
+$routes->get('bookings', 'Bookings::index');
+$routes->get('/bookings/create/(:num)', 'Bookings::create/$1');
+$routes->get('/bookings/update/(:num)/(:alpha)', 'Bookings::updateStatus/$1/$2');
+
+
+
+
