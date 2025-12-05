e9a19c2798711f7e980427cbbca70de747009cd4
JustinSr28 <sanchoruizjustin@gmail.com>
2025-12-04 19:52:34 -0600

    Fix routes of logo and user images

--- app/Views/layouts/headerSearchPublic.php
+++ app/Views/layouts/headerSearchPublic.php
@@ -1,5 +1,5 @@
 <header>
-    <img src="<?= base_url('images/logo.png') ?>" class="design-logo" alt="Aventones Logo">
+    <img src="<?= base_url('uploads/logo.png') ?>" class="design-logo" alt="Aventones Logo">
 
     <div class="menu-cont">
         <nav class="Head" aria-label="Main menu">
@@ -9,7 +9,7 @@
 
         <div class="navigation-cont">
             <div class="user-menu">
-                <img src="<?= base_url('images/user.png') ?>" class="navigation-image" alt="User icon">
+                <img src="<?= base_url('uploads/user.png') ?>" class="navigation-image" alt="User icon">
                 <nav class="menu-hover">
                     <ul>
                         <li><a href="<?= base_url('login') ?>">Login</a></li>
